86c3e4023d95be431ebf5b1303172db6d7579562
CPK <cpkaq@outlook.com>
2023-07-03 15:30:06 +0000

    update get.js

--- get.js
+++ get.js
@@ -2,9 +2,6 @@ let events;
 let entryList = document.getElementById("entry-list");
 
 function handleDate() {
-    var selectedDate = document.getElementById("datepicker").value;
-    // console.log("你选择的日期是：" + selectedDate);
-
     var date = new Date(document.getElementById("datepicker").value);
     var month = date.getMonth();    
     var day = date.getDate();
@@ -27,7 +24,6 @@ function handleDate() {
 }
 
 $(document).ready(function() {
-    $(".no-entry").hide();
     var today = new Date().toISOString().split('T')[0];
     $("#datepicker")[0].value = today;
 
@@ -37,6 +33,7 @@ $(document).ready(function() {
         // 在这里处理读取到的JSON数据
         events = data
         // console.log(events);
+        handleDate();
         })
         .catch(error => {
         // 处理读取错误
